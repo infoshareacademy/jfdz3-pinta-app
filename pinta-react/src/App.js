ef93851de865ce2e6b0ba81c9991e72b017379f7
Kris Garski <kgarski@gmail.com>
2017-02-26 20:03:14 +0100

    react app init

--- pinta-react/src/App.js
+++ pinta-react/src/App.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from 'react-bootstrap/lib/Button';
+import Modal from 'react-bootstrap/lib/Modal';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Button />
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to React  PINTA</h2>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
