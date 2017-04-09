3f710a4c74334b642443cfcd1b9b3c7292b3bad2
enea <jorgji.enea@gmail.com>
2017-04-09 12:23:46 +0200

    login

--- pinta-react/src/pages_layouts/login/login.js
+++ pinta-react/src/pages_layouts/login/login.js
@@ -1,29 +1,82 @@
-/**
- * Created by enea on 02.04.17.
- */
 
 import React from 'react';
-import {Row} from 'react-bootstrap';
+// import ReactDOM from 'react-dom';
+import GoogleLogin from 'react-google-login';
+import FontAwesome from 'react-fontawesome'
 
+const responseGoogle = (response) => {
+    console.log(response);
+}
 
 class Login extends React.Component {
 
 
+
     render() {
-
-
         return (
-
-            <Row>
-                <div className="login">
-                    <h1>Loguj się i sprawdź wydarzenia!</h1>
+            <div className="login">
+                <h1>Zaloguj się i sprawdź wydarzenia!</h1>
+                <div className="google-login">
+                    <GoogleLogin
+                        clientId={'209101981816-mfc1a3pfpvg6dncfuvbcbtldnqnaoamr.apps.googleusercontent.com'}
+                        onSuccess={show name}
+                        onFailure={responseGoogle}
+                        offline={false}
+                    >
+                        <FontAwesome
+                            name='google'
+                        />
+                        <span> Zaloguj się z Google</span>
+                    </GoogleLogin>
                 </div>
-            </Row>
-
+            </div>
         )
     }
-
 }
 
 
 export default Login;
+
+
+
+
+// import React from 'react';
+// import {Row} from 'react-bootstrap';
+// import ReactDOM from 'react-dom';
+// import GoogleLogin from 'react-google-login';
+//
+// class Login extends React.Component {
+//     const responseGoogle = (response) => {
+//     console.log(response);
+// }
+//
+//     ReactDom.render() {
+//
+//
+//         return (
+//
+//             <Row>
+//                 <div className="login">
+//                     <h1>Loguj się i sprawdź wydarzenia!</h1>
+//                 </div>
+//             </Row>
+//
+//
+//         )
+//     }
+//
+// }
+// export default Login;
+//
+//
+//
+//
+// ReactDOM.render(
+//     <GoogleLogin
+//         clientId="209101981816-mfc1a3pfpvg6dncfuvbcbtldnqnaoamr.apps.googleusercontent.com"
+//         buttonText="Login"
+//         onSuccess={responseGoogle}
+//         onFailure={responseGoogle}
+//     />,
+//     document.getElementById('googleButton')
+// );
