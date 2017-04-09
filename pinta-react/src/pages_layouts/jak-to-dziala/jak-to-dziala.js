b1c07c43896a915ce039811e07a7502f251e113c
enea <jorgji.enea@gmail.com>
2017-04-09 11:05:51 +0200

    how_works

--- pinta-react/src/pages_layouts/jak-to-dziala/jak-to-dziala.js
+++ pinta-react/src/pages_layouts/jak-to-dziala/jak-to-dziala.js
@@ -3,7 +3,7 @@
  */
 
 import React from 'react';
-import {Col, Thumbnail,Grid, Row} from 'react-bootstrap';
+import {Col,Grid, Row} from 'react-bootstrap';
 import FaCalendarCheckO from 'react-icons/lib/fa/calendar-check-o';
 import FaShareSquareO from 'react-icons/lib/fa/share-square-o';
 import FaMobile from 'react-icons/lib/fa/mobile';
@@ -11,32 +11,28 @@ import FaMobile from 'react-icons/lib/fa/mobile';
 class Thumbnails extends React.Component {
     render() {
         return (
+        <div className="how_works">
             <Grid>
-                <Row className="how_works">
-                    <h1>Jak to działą?</h1>
+                <h1>Jak to działą?</h1>
+                <Row className="show-grid">
                     <Col xs={6} md={4}>
-                        <Thumbnail>
-                            <div className="feature-item"><FaCalendarCheckO/></div>
-                            <h3>Kalendarz</h3>
-                            <p>Nasza aplikacja oferuje korzystanie z osobistego kalendarza eventów, który pozwala na kategoryzowanie wydarzeń i ustawianie przypomnień. Dzięki temu łatwiej jest zaplanować swój czas, czy z odpowiednim wyprzedzeniem kupić bilety.</p>
-                        </Thumbnail>
+                        <div className="feature-item"><FaCalendarCheckO/></div>
+                        <h3>Kalendarz</h3>
+                        <p>Nasza aplikacja oferuje korzystanie z osobistego kalendarza eventów, który pozwala na kategoryzowanie wydarzeń i ustawianie przypomnień. Dzięki temu łatwiej jest zaplanować swój czas, czy z odpowiednim wyprzedzeniem kupić bilety.</p>
                     </Col>
                     <Col xs={6} md={4}>
-                        <Thumbnail>
-                            <div className="feature-item"><FaShareSquareO/></div>
+                        <div className="feature-item"><FaShareSquareO/></div>
                         <h3>Kryteria</h3>
-                            <p>Wyszukiwanie wydarzeń odbywa się dzięki zastosowaniu odpowiednich kryteriów, opisujących rodzaj i charakter imprezy (np. impreza biznesowa, koncert, festiwal na otwartym powietrzu itd.).</p>
-                        </Thumbnail>
+                        <p>Wyszukiwanie wydarzeń odbywa się dzięki zastosowaniu odpowiednich kryteriów, opisujących rodzaj i charakter imprezy (np. impreza biznesowa, koncert, festiwal na otwartym powietrzu itd.).</p>
                     </Col>
                     <Col xs={6} md={4}>
-                        <Thumbnail>
-                            <div className="feature-item"><FaMobile/></div>
-                            <h3>Login</h3>
-                            <p>Wszystkie informacje przechowywane są w profilu użytkownika, do którego zastosowano łatwy i bezpieczny system logowania przez portale społecznościowe, takie jak Facebook, czy Google. Nie wymaga on tworzenia i zapamiętywania dodatkowych haseł.</p>
-                        </Thumbnail>
+                        <div className="feature-item"><FaMobile/></div>
+                        <h3>Login</h3>
+                        <p>Wszystkie informacje przechowywane są w profilu użytkownika, do którego zastosowano łatwy i bezpieczny system logowania przez portale społecznościowe, takie jak Facebook, czy Google. Nie wymaga on tworzenia i zapamiętywania dodatkowych haseł.</p>
                     </Col>
                 </Row>
             </Grid>
+        </div>
         );
     }
 }
